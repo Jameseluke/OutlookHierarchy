7a1c65aa8b95073f733a35e454ccc43e92e485f1
James Luke <JLuke1@student.unimelb.edu.au>
2017-02-07 14:08:32 +1100

    Now fetches employees in bulk

--- main.sh
+++ main.sh
@@ -1,7 +1,23 @@
-function getEmployeeByName{
-  $query = $args[0]
+function getEmployeesByName{
+  # $query contains the names of the employees to search for in format "LastName, FirstName"
+  $queries = $args[0]
+  # if $correct is true, will not check if query and result match
+  $correct = $args[1]
+  $temp = New-Object System.Collections.ArrayList
   $outlook = New-Object -ComObject Outlook.Application
-  return $outlook.Session.GetGlobalAddressList().AddressEntries.Item($query)
+  foreach($query in $queries){
+    $emp = $outlook.Session.GetGlobalAddressList().AddressEntries.Item($query)
+    if(!$correct){
+      $match = checkMatch $query $emp.Name
+    }
+    if(!$correct -and ($match -eq -1)){
+      $failure += " $query`r`n"
+    }
+    else {
+      $temp.Add($emp) | Out-Null
+    }
+  }
+  return $temp
 }
 
 function checkMatch{
@@ -20,29 +36,23 @@ function main{
   param(
         [switch] $correct)
   $queries = Get-Content $args[0]
+  $unproccessedEmp = getEmployeesByName $queries $correct
   $success = "Name, UID, Department, Role, Manager`r`n"
   $failure = "Names without matches:`r`n"
   $employees = @{}
-  foreach($query in $queries){
-    $employee = getEmployeeByName $query
-    if(!$correct){
-      $match = checkMatch $query $employee.Name
-    }
-    if(!$correct -and ($match -eq -1)){
-      $failure += " $query`r`n"
-    }
-    else {
-      $full = $employee.getExchangeUser()
-      while($full.Alias -and !$employees.ContainsKey($full.Alias)){
-        $temp = @{}
-        $alias = $full.Alias
-        $temp.Add("Name", "`"" + $full.Name +"`"")
-        $temp.Add("Department", "`"" + $full.Department +"`"")
-        $temp.Add("Role", "`"" + $full.JobTitle +"`"")
-        $full = $full.Manager
-        $temp.Add("Manager", $full.Alias)
-        $employees.Add($alias, $temp)
-      }
+  cls
+  Write-Progress -Activity "Working..."
+  foreach($emp in $unproccessedEmp){
+    $full = $emp.getExchangeUser()
+    while($full.Alias -and !$employees.ContainsKey($full.Alias)){
+      $temp = @{}
+      $alias = $full.Alias
+      $temp.Add("Name", "`"" + $full.Name +"`"")
+      $temp.Add("Department", "`"" + $full.Department +"`"")
+      $temp.Add("Role", "`"" + $full.JobTitle +"`"")
+      $full = $full.Manager
+      $temp.Add("Manager", $full.Alias)
+      $employees.Add($alias, $temp)
     }
   }
   foreach ($h in $employees.Keys) {
@@ -52,5 +62,6 @@ function main{
     $success += $employees.$h.Role  + ","
     $success += $employees.$h.Manager + "`n"
   }
-  $success | Out-File -encoding ASCII hierarchy.txt
+  $success | Out-File -encoding ASCII $args[1]
+  echo "Finished, Output saved to $args[1]"
 }
