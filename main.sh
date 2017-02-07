dcf3787412f61c3f41d0d496d9a74dee87696128
James Luke <JLuke1@student.unimelb.edu.au>
2017-02-07 16:43:52 +1100

    Finished majority of commenting

--- main.sh
+++ main.sh
@@ -1,34 +1,50 @@
 function getEmployeesByName{
   # $query contains the names of the employees to search for in format "LastName, FirstName"
   $queries = $args[0]
-  # if $correct is true, will not check if query and result match
+  # if $correctflag is true, will not check if query and result match
   $correctflag = $args[2]
+  # $logfile will be written to in the event of no match being found
   $logfile = $args[1]
+  # $failure intially empty so that it can be tested in an if statement
   $failure = ""
+  # $temp is an arraylist of all employees that were successfully matched
   $temp = New-Object System.Collections.ArrayList
+  # If outlook is not open, then script cannot access global address list
   $outlook = New-Object -ComObject Outlook.Application
+  if(!$outlook){
+    Write-Host "ERROR: Please ensure Outlook is open and connected to the server"
+    exit
+  }
   foreach($query in $queries){
+    # create temp correct flag so that it can be changed for each query
     $correct = $correctflag
     if ($correct){
+      # Assumes the first employee found is correct, does no checking with user
       $emp = $outlook.Session.GetGlobalAddressList().AddressEntries.Item($query)
     }
     while(!$correct){
       $emp = $outlook.Session.GetGlobalAddressList().AddressEntries.Item($query)
       $match = checkMatch $query $emp.Name
-      if($match -eq -1){
+      if(!$match){
+        # keep name in case user does not wish to search further
         $nomatch = $query
         $query = Read-Host "Enter a new name to search for (Leave blank to skip)"
+        # if a new query is not submitted, skip this employee
         if (!$query){
+          # This will cause an error message to be printed at the end of script
           $failure += " $nomatch`r`n"
           break
         }
       }
-      $correct = if ($match -eq -1) {$FALSE} else {$TRUE}
+      # Check to see if loop should be broken or not
+      $correct = if (!$match) {$FALSE} else {$TRUE}
     }
+    # if a match was found, add employee to output arraylist
     if ($correct){
       $temp.Add($emp) | Out-Null
     }
   }
+  # If there was one or more names without match, write their names to log file
   if($failure){
     $errorString = "Names without matches:`r`n" + $failure
     $errorString | Out-File -encoding ASCII $logfile
@@ -36,23 +52,27 @@ function getEmployeesByName{
   return $temp
 }
 
+# Return 1 if user thinks the two strings match, else 0
 function checkMatch{
   $string1 = $args[0]
   $string2 = $args[1]
   $confirmation = Read-Host "$string1 == $string2 [y/n]"
   while($confirmation -ne "y")
   {
-      if ($confirmation -eq 'n') {return -1}
+      if ($confirmation -eq 'n') {return 0}
       $confirmation = Read-Host "Please choose one of the available options [y/n]"
   }
-  return 0
+  return 1
 }
 
 function main{
   param(
+        # correct switch causes script to ignore input validation
         [switch] $correct)
+  # List of all names to search from
   $queries = Get-Content $args[0]
   $log = [System.IO.Path]::GetTempFileName()
+  # list of all employees which were matched to input names
   $unproccessedEmp = getEmployeesByName $queries $log $correct
   $success = "Name, UID, Department, Role, Manager`r`n"
   $employees = @{}
@@ -60,6 +80,9 @@ function main{
   Write-Progress -Activity "Working..."
   foreach($emp in $unproccessedEmp){
     $full = $emp.getExchangeUser()
+    # Create an object containing all details about the employee
+    # Object is referenced by alias within the employees object
+    # Stops when employee has no manager or manager has already been found before
     while($full.Alias -and !$employees.ContainsKey($full.Alias)){
       $temp = @{}
       $alias = $full.Alias
@@ -71,6 +94,7 @@ function main{
       $employees.Add($alias, $temp)
     }
   }
+  # format for CSV output
   foreach ($h in $employees.Keys) {
     $success += $employees.$h.Name + ","
     $success += $h  + ","
@@ -81,6 +105,7 @@ function main{
   $out = $args[1]
   $success | Out-File -encoding ASCII $out
   Write-Host "Finished, Output saved to $out"
+  # Print errors to screen and delete log file
   Get-Content $log | Write-Host
   Get-Item $log | Remove-Item
 }
